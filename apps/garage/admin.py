5c4ebc957c4ab40351ce5674f003926e81ae2c93
Sushkov AS <aveslaizer@gmail.com>
2023-09-11 17:17:45 +0300

    Настроил таблицы связанные с транспортом

--- apps/garage/admin.py
+++ apps/garage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import FuelType, TransmissionType, DriveType, Transport
+from .models import FuelType, TransmissionType, DriveType, Transport, TransportCategory
 
 admin.site.register(FuelType)
 admin.site.register(TransmissionType)
 admin.site.register(DriveType)
+admin.site.register(TransportCategory)
 admin.site.register(Transport)
--- apps/garage/models.py
+++ apps/garage/models.py
@@ -5,24 +5,48 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class FuelType(models.Model):
-    name = models.CharField(unique=True, max_length=20)
+    fuel_type = models.CharField(primary_key=True, max_length=20, verbose_name="Топливо")
+
+    def __str__(self):
+        return self.fuel_type
 
 
 class TransmissionType(models.Model):
-    gearbox_type = models.CharField(unique=True, max_length=20)
+    gearbox_type = models.CharField(primary_key=True, max_length=20, verbose_name="КПП")
+
+    def __str__(self):
+        return self.gearbox_type
 
 
 class DriveType(models.Model):
-    drive_type = models.CharField(unique=True, max_length=20)
+    drive_type = models.CharField(primary_key=True, max_length=20, verbose_name="Привод")
+
+    def __str__(self):
+        return self.drive_type
+
+
+class TransportCategory(models.Model):
+    category = models.CharField(primary_key=True, max_length=20, verbose_name="Категория")
+
+    def __str__(self):
+        return self.category
 
 
 class Transport(models.Model):
-    brand = models.CharField(default="Марка", max_length=30)
-    model = models.CharField(default="Модель", max_length=30)
+    brand = models.CharField(default="Марка", max_length=30, verbose_name="Марка")
+    model = models.CharField(default="Модель", max_length=30, verbose_name="Модель")
     year = models.IntegerField(default=datetime.now().year,
-                               validators=[MinValueValidator(1900), MaxValueValidator(datetime.now().year)])
-    engine_volume = models.FloatField(default=1.6, validators=[MinValueValidator(0.0001), MaxValueValidator(100.0)])
-    odometer = models.IntegerField(default=12345, null=False, validators=[MinValueValidator(1)])
+                               validators=[MinValueValidator(1900), MaxValueValidator(datetime.now().year)],
+                               verbose_name="Год выпуска")
+    engine_volume = models.FloatField(default=1.6, validators=[MinValueValidator(0), MaxValueValidator(100.0)],
+                                      verbose_name="Объем двигателя, л")
+    engine_power = models.FloatField(default=1, validators=[MinValueValidator(0.1)], verbose_name="Мощность, л/с")
+    odometer = models.IntegerField(default=12345, null=False, validators=[MinValueValidator(1)],
+                                   verbose_name="Пробег, км")
     fuel_type = models.ForeignKey(FuelType, null=True, on_delete=models.SET_NULL)
     transmission_type = models.ForeignKey(TransmissionType, null=True, on_delete=models.SET_NULL)
     drive_type = models.ForeignKey(DriveType, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(TransportCategory, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} {self.year}г"
