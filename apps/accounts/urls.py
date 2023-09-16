6da32280738831ee26319867136c1b87bfc97688
Sushkov AS <aveslaizer@gmail.com>
2023-09-16 22:16:56 +0300

    РÐРефакторинг в приложении accounts

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.accounts.views import registration
+from apps.accounts.views import Accounts
 
 urlpatterns = [
-    path('registration/', registration, name='registration'),
+    path('registration/', Accounts.registration, name='registration'),
+    path('registration_done/', Accounts.registration_done, name='registration_done'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,17 +3,21 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.forms import UserCreationForm
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            user_group = Group.objects.get(name='common user')
-            user.groups.add(user_group)
-            return redirect('main')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/registration.html', {'form': form})
+class Accounts:
+    @staticmethod
+    def registration(request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.save()
+                user_group = Group.objects.get(name='common user')
+                user.groups.add(user_group)
+                return redirect('registration_done')
+        else:
+            form = UserCreationForm()
+        return render(request, 'accounts/registration.html', {'form': form})
 
-# TODO дальше разбираться с регистрацией пользователей
+    @staticmethod
+    def registration_done(request):
+        return render(request, 'accounts/registration_done.html')
