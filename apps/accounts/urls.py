e4e6794e5c18ba6de15702e607ad24bb501878ec
Sushkov AS <aveslaizer@gmail.com>
2023-09-19 19:33:36 +0300

    Рефакторинг

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,5 +3,6 @@ from apps.accounts.views import LoginUser, RegisterUser
 
 urlpatterns = [
     path('registration/', RegisterUser.as_view(), name='registration'),
-    path('login/', LoginUser.as_view(), name='login')
+    path('login/', LoginUser.as_view(), name='login'),
+    #path('profile/', )
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,17 +1,33 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import request
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+DEFAULT_GROUP_NAME = 'common user'
 
-class RegisterUser(CreateView):
+
+class RegisterUser(SuccessMessageMixin, CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/registration.html'
+    success_message = "all_ok"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.save()
+        user_group = Group.objects.get(name=DEFAULT_GROUP_NAME)
+        user.groups.add(user_group)
+        mess = self.get_success_message(form.cleaned_data)
+        print(mess)
+        # messages.success(request, 'Form submission successful')
+        return redirect(self.get_success_url())
 
     def get_success_url(self):
-        return reverse_lazy('login')
+        return 'login'
 
 
 class LoginUser(LoginView):
@@ -20,3 +36,5 @@ class LoginUser(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('main')
+
+# TODO попробовать разобраться с всплывающими сообщениями
