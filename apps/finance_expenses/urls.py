e588767364770dcead06681bb2d85049ce27f3e2
Sushkov AS <aveslaizer@gmail.com>
2023-10-04 22:47:24 +0300

    Реализовал вывод всех трат на ТС в виде таблицы

--- apps/finance_expenses/urls.py
+++ apps/finance_expenses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddFinanceExpenses
+from .views import AddFinanceExpenses, ShowFinanceExpenses
 
 urlpatterns = [
     path('add_expense/', AddFinanceExpenses.as_view(), name='add_expense'),
+    path('expenses_table/', ShowFinanceExpenses.as_view(), name='expenses_table'),
 ]
--- apps/finance_expenses/views.py
+++ apps/finance_expenses/views.py
@@ -1,8 +1,9 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import AddFinanceExpensesForm
 from ..garage.models import Transport
+from .models import FinanceExpense
 import datetime
 
 
@@ -42,3 +43,28 @@ class AddFinanceExpenses(SuccessMessageMixin, CreateView):
 
     def get_success_url(self):
         return reverse_lazy('garage')
+
+
+class ShowFinanceExpenses(ListView):
+    model = FinanceExpense
+    context_object_name = 'expenses'
+    template_name = 'finance_expenses/expenses_table.html'
+
+    def get_transport(self):
+        transport_id = self.request.GET.get('transport_id')
+        self.transport_obj = Transport.objects.get(pk=transport_id)
+
+    def get_queryset(self):
+        queryset = FinanceExpense.objects.filter(transport=self.request.GET.get('transport_id'))
+        self.queryset = queryset
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        initial = super(ShowFinanceExpenses, self).get_context_data(**kwargs)
+        initial['header'] = [
+            field.verbose_name for field in self.model._meta.get_fields() if field.verbose_name != 'Транспорт'
+        ]
+        initial['rows'] = list(self.queryset.values('id', 'summ', 'date', 'odometer', 'expense_type', 'add_date'))
+        self.get_transport()
+        initial['transport_name'] = self.transport_obj
+        return initial
